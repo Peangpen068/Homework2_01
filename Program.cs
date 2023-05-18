7260e89b5c0281c6031f078b5aad71938ba5c549
Peangpen068 <123537577+Peangpen068@users.noreply.github.com>
2023-05-19 03:20:35 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of rows for Pascal's triangle: ");
+        int numRows;
+
+        while (!int.TryParse(Console.ReadLine(), out numRows) || numRows < 0)
+        {
+            Console.WriteLine("Invalid Pascal's triangle row number. Please enter a non-negative integer.");
+            Console.Write("Enter the number of rows for Pascal's triangle: ");
+        }
+
+        Console.WriteLine("Pascal's triangle:");
+        for (int i = 0; i <= numRows; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                int coef = CalculateCoefficient(i, j);
+                Console.Write(coef + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static int CalculateCoefficient(int row, int column)
+    {
+        if (column == 0 || column == row)
+            return 1;
+        else
+            return CalculateCoefficient(row - 1, column - 1) + CalculateCoefficient(row - 1, column);
+    }
+}
